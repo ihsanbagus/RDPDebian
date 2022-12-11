64d7008ee6f47b7e7a37cc88916ef37f3b12b346
ihsanbagus <32384214+ihsanbagus@users.noreply.github.com>
2022-12-11 21:37:37 +0700

    Update RDP-Debian.sh

--- RDP-Debian.sh
+++ RDP-Debian.sh
@@ -3,19 +3,19 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m FSID
 sudo useradd -m AGID
-sudo adduser AGID
-sudo echo 'AGID:1234' | sudo chpasswd
-sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt update
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo adduser AGID sudo
+echo 'AGID:1234' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo apt-get update
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-sudo apt install --assume-yes xfce4 desktop-base
+apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
@@ -27,7 +27,7 @@ printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debia
 read -p "Paste Here: " CRP
 su - AGID -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt upgrade &> /dev/null
+if sudo apt-get upgrade &> /dev/null
 then
     printf "\n\nUpgrade Completed." >&2
 else
