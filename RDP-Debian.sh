37d54bfd10a89c8d917243ddd3cea250dbb0f7fb
ihsanbagus <32384214+ihsanbagus@users.noreply.github.com>
2022-12-11 21:42:22 +0700

    Update RDP-Debian.sh

--- RDP-Debian.sh
+++ RDP-Debian.sh
@@ -6,7 +6,7 @@ sudo useradd -m AGID
 sudo adduser AGID sudo
 echo 'AGID:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
+sudo apt-get update \
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -14,7 +14,7 @@ sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
+sudo systemctl disable lightdm.service \
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
